c4f975152a65538ef2946020d6b8a3f91bb67f4c
Alexander <alexander.kilyushin@gmail.com>
2022-02-08 18:12:35 +0300

    Add examples and comments to keyof

--- keyof.ts
+++ keyof.ts
@@ -1,25 +1,45 @@
 /* The `keyof` keyword returns a union of the property names of a type. */
 
-/** Example 1 */
-type User = { age: number; name: string }
+/* Example 1 */
+type User = {
+  age: number
+  name: string
+}
 
-type UserKey = keyof User
+type UserKey = keyof User // "age" | "name"
+type UserValue = User[keyof User] // number | string
 
-const aKeyOfUser: UserKey = "age"
-const anotherKeyOfUser: UserKey = "name"
+/* Example 2 */
+const user1 = {
+  age: 25,
+  name: "John",
+}
 
-/** Example 2 */
+type UserValue1 = typeof user1[keyof typeof user1] // number | string
+
+/* Example 3 */
+const user2 = {
+  age: 25,
+  name: "John",
+} as const
+
+type UserValue2 = typeof user2[keyof typeof user2] // 25 | "John"
+
+/* Example 4 */
 /**
  * The `keyof` keyword can be used to constrain generic type parameters
  * so that they can only be typed to match the properties of another type.
  */
-type LogObjectValueByKey = <T, K extends keyof T>(object: T, key: K) => void
+type LogObjectValueByKey = <T, K extends keyof T>(object: T, key: K) => T[K]
 
-const logObjectValueByKey: LogObjectValueByKey = (object, key): void => {
-  console.log(object[key])
+const getObjectValueByKey: LogObjectValueByKey = (object, key) => {
+  return object[key]
 }
 
-const car1 = { model: "Ford", year: 1960 }
+const car1 = {
+  model: "Ford",
+  year: 1960,
+}
 
 // The second argument should be either "model" or "year".
 logObjectValueByKey(car1, "model")
