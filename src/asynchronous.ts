5710fd08cff24beca0184d391c20aaa94f64222c
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2022-03-25 23:54:32 +0400

    Create asynchronous.ts

---
+++ src/asynchronous.ts
@@ -0,0 +1,18 @@
+// string type is in Promise generic here.
+const promiseReturningString = (): Promise<string> => {
+  // And the argument to resolve callback is also of type string.
+  return new Promise((resolve: (outputValue: string) => void, reject: (error: Error) => void) => {
+    // And we pass an argument of type string to the resolve callback.
+    resolve("Resolved!")
+    reject(new Error("Oops!"))
+  })
+}
+
+promiseReturningString()
+  .then((resolvedValue) => {
+    console.log(resolvedValue)
+  })
+  .catch(({ message, name }) => {
+    console.log(name)
+    console.log(message)
+  })
