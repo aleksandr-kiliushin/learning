67091262f578f15a49ec40349bddef484e6679e4
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-02 09:59:57 +0400

    feat: Add findMinValuePromisified example

--- src/javascript/promisify/index.test.ts
+++ src/javascript/promisify/index.test.ts
@@ -1,15 +1,48 @@
-import { add, promisify } from "./index"
+import { promisify, TFunctionToBePromisified } from "./index"
 
-describe("'add' works correctly", () => {
-  test("default, with callback", () => {
-    add({ a: 1, b: 2 }, (result) => {
+describe("promisify", () => {
+  describe("'add' function case", () => {
+    const add: TFunctionToBePromisified<{ a: number; b: number }, number> = ({ a, b }, onDone) => {
+      setTimeout(() => {
+        const result = a + b
+        onDone(result)
+      }, 0)
+    }
+
+    test("add works correctly", () => {
+      add({ a: 1, b: 2 }, (result) => {
+        expect(result).toEqual(3)
+      })
+    })
+
+    test("addPromisified works correctly", async () => {
+      const addPromisified = promisify(add)
+      const result = await addPromisified({ a: 1, b: 2 })
       expect(result).toEqual(3)
     })
   })
 
-  test("promisified", async () => {
-    const addPromisified = promisify(add)
-    const result = await addPromisified({ a: 1, b: 2 })
-    expect(result).toEqual(3)
+  describe("'findMinValue' function case", () => {
+    const findMinValue: TFunctionToBePromisified<{ values: number[] }, number> = ({ values }, onDone) => {
+      setTimeout(() => {
+        const result = values.reduce((accumulator, currentValue) => {
+          if (currentValue < accumulator) return currentValue
+          return accumulator
+        })
+        onDone(result)
+      }, 0)
+    }
+
+    test("findMinValue works correctly", () => {
+      findMinValue({ values: [4, 5, 2, 8] }, (result) => {
+        expect(result).toEqual(2)
+      })
+    })
+
+    test("findMinValuePromisified works correctly", async () => {
+      const findMinValuePromisified = promisify(findMinValue)
+      const result = await findMinValuePromisified({ values: [4, 5, 2, 8] })
+      expect(result).toEqual(2)
+    })
   })
 })
--- src/javascript/promisify/index.ts
+++ src/javascript/promisify/index.ts
@@ -1,14 +1,7 @@
-type TFunctionToBePromisified<TLogicArgument extends Record<string, unknown>, TResult extends unknown> = (
+export type TFunctionToBePromisified<TLogicArgument extends Record<string, unknown>, TResult extends unknown> = (
   logicArgument: TLogicArgument,
   onDone: (result: TResult) => void
 ) => void
-
-export const add: TFunctionToBePromisified<{ a: number; b: number }, number> = ({ a, b }, onDone) => {
-  setTimeout(() => {
-    const result = a + b
-    onDone(result)
-  }, 0)
-}
 
 export function promisify<TLogicArgument extends Record<string, unknown>, TResult extends unknown>(
   functionToBePromisified: TFunctionToBePromisified<TLogicArgument, TResult>
