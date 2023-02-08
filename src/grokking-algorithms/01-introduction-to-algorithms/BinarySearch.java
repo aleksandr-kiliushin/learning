5065847f2d07932898e25edde17723cb71198bfd
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-08 21:18:37 +0400

    grokking-algorithms: BinarySearch: Add NumberSearcher, BinarySearch

--- src/grokking-algorithms/01-introduction-to-algorithms/BinarySearch.java
+++ src/grokking-algorithms/01-introduction-to-algorithms/BinarySearch.java
@@ -1,11 +1,39 @@
 /**
  * How to run the example:
- * 1. Compile: javac ./src/grokking-algorithms/01-introduction-to-algorithms/BinarySearch.java
- * 2. Execute: java --class-path ./src/grokking-algorithms/01-introduction-to-algorithms BinarySearch
+ * javac ./src/grokking-algorithms/01-introduction-to-algorithms/BinarySearch.java && java --class-path ./src/grokking-algorithms/01-introduction-to-algorithms BinarySearch
  */
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+class NumberSearcher {
+  public static Integer find(int[] elements, int elementToFindIndexOf) {
+    int indexToSearchFrom = 0;
+    int indexToSearchTo = elements.length;
+    int middleIndex;
+    int elementAtMiddleIndex;
+
+    while (indexToSearchFrom <= indexToSearchTo) {
+      middleIndex = indexToSearchFrom + (indexToSearchTo - indexToSearchFrom) / 2;
+      elementAtMiddleIndex = (int) Array.get(elements, middleIndex);
+      if (elementAtMiddleIndex == elementToFindIndexOf) {
+        return middleIndex;
+      } else if (elementAtMiddleIndex < elementToFindIndexOf) {
+        indexToSearchFrom = middleIndex + 1;
+      } else if (elementAtMiddleIndex > elementToFindIndexOf) {
+        indexToSearchTo = middleIndex - 1;
+      }
+    }
+
+    return null;
+  }
+}
 
 class BinarySearch {
   public static void main(String[] args) {
-    System.out.println("Hello from binary search."); 
+    int[] elements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    Integer elementIndex = NumberSearcher.find(elements, 9);
+    System.out.println(elementIndex);
   }
 }
