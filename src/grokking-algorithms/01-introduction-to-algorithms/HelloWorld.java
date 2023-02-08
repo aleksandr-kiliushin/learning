004fc85ab210ec32d87490dbf3e6aab0e8a0a635
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-08 10:09:56 +0400

    grokking-algorithms: Add HelloWorld.java

---
+++ src/grokking-algorithms/01-introduction-to-algorithms/HelloWorld.java
@@ -0,0 +1,8 @@
+// javac ./src/grokking-algorithms/01-introduction-to-algorithms/HelloWorld.java
+// java --class-path ./src/grokking-algorithms/01-introduction-to-algorithms HelloWorld
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!"); 
+  }
+}
