370f9a5149431dd9674a410ebcdb93b4e90a2e53
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-24 21:28:42 +0400

    Refactor binary search tests

--- src/algorithms/binary-search/BinarySearchTest.java
+++ src/algorithms/binary-search/BinarySearchTest.java
@@ -9,67 +9,56 @@ class BinarySearchTest {
 
   @Test
   public void testDoesBinarySearchFind1() {
-    Integer elementIndex = BinarySearch.find(this.elements, 1);
-    Assertions.assertEquals(elementIndex, 0);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 1), 0);
   }
 
   @Test
   public void testDoesBinarySearchFind2() {
-    Integer elementIndex = BinarySearch.find(this.elements, 2);
-    Assertions.assertEquals(elementIndex, 1);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 2), 1);
   }
 
   @Test
   public void testDoesBinarySearchFind3() {
-    Integer elementIndex = BinarySearch.find(this.elements, 3);
-    Assertions.assertEquals(elementIndex, 2);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 3), 2);
   }
 
   @Test
   public void testDoesBinarySearchFind4() {
-    Integer elementIndex = BinarySearch.find(this.elements, 4);
-    Assertions.assertEquals(elementIndex, 3);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 4), 3);
   }
 
   @Test
   public void testDoesBinarySearchFind5() {
-    Integer elementIndex = BinarySearch.find(this.elements, 5);
-    Assertions.assertEquals(elementIndex, 4);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 5), 4);
   }
 
   @Test
   public void testDoesBinarySearchFind6() {
-    Integer elementIndex = BinarySearch.find(this.elements, 6);
-    Assertions.assertEquals(elementIndex, 5);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 6), 5);
   }
 
   @Test
   public void testDoesBinarySearchFind7() {
-    Integer elementIndex = BinarySearch.find(this.elements, 7);
-    Assertions.assertEquals(elementIndex, 6);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 7), 6);
   }
 
   @Test
   public void testDoesBinarySearchFind8() {
-    Integer elementIndex = BinarySearch.find(this.elements, 8);
-    Assertions.assertEquals(elementIndex, 7);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 8), 7);
   }
 
   @Test
   public void testDoesBinarySearchFind9() {
-    Integer elementIndex = BinarySearch.find(this.elements, 9);
-    Assertions.assertEquals(elementIndex, 8);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 9), 8);
   }
 
   @Test
   public void testDoesBinarySearchFind10() {
-    Integer elementIndex = BinarySearch.find(this.elements, 10);
-    Assertions.assertEquals(elementIndex, 9);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 10), 9);
   }
 
   @Test
   public void testDoesBinarySearchFind100500() {
-    Integer elementIndex = BinarySearch.find(this.elements, 100500);
-    Assertions.assertEquals(elementIndex, -1);
+    Assertions.assertEquals(BinarySearch.find(this.elements, 100500), -1);
   }
 }
