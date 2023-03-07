7bce0ecc34747139360c8c6f50da43b7f0df86c9
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-07 19:42:44 +0400

    algorithms: Implement quick sort

--- src/algorithms/quick-sort/QuickSort.java
+++ src/algorithms/quick-sort/QuickSort.java
@@ -1,5 +1,41 @@
 class QuickSort {
+  public static int partition(int[] elements, int startIndex, int endIndex) {
+    if (startIndex >= endIndex) {
+      return startIndex;
+    }
+
+    int i = startIndex - 1;
+    int j = startIndex;
+
+    while (j < endIndex) {
+      if (elements[j] >= elements[endIndex]) {
+        j++;
+        continue;
+      }
+
+      i++;
+      int elementAtIndexI = elements[i];
+      int elementAtIndexJ = elements[j];
+      elements[i] = elementAtIndexJ;
+      elements[j] = elementAtIndexI;
+      j++;
+    }
+
+    i++;
+
+    int elementAtIndexI = elements[i];
+    int elementAtEndIndex = elements[endIndex];
+    elements[i] = elementAtEndIndex;
+    elements[endIndex] = elementAtIndexI;
+
+    QuickSort.partition(elements, startIndex, i - 1);
+    QuickSort.partition(elements, i + 1, endIndex);
+
+    return i;
+  }
+
   public static int[] sort(int[] elements) {
-    return new int[0];
+    QuickSort.partition(elements, 0, elements.length - 1);
+    return elements;
   }
 }
