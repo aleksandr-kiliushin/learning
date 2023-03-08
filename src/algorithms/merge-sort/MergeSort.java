e0d0e48ab839372933cc2fc8fec1379ec31e8973
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-08 19:25:36 +0400

    algorithms: Add merge sort

--- src/algorithms/merge-sort/MergeSort.java
+++ src/algorithms/merge-sort/MergeSort.java
@@ -1,5 +1,61 @@
 class MergeSort {
+  public static int[][] split(int[] elements) {
+    int indexToSplitBy = elements.length / 2;
+
+    int[] firstArray  = new int[indexToSplitBy];
+    for (int index = 0; index < indexToSplitBy; index++) {
+      firstArray[index] = elements[index];
+    }
+
+    int[] secondArray = new int[elements.length - indexToSplitBy];
+    for (int index = 0; index < elements.length - indexToSplitBy; index++) {
+      secondArray[index] = elements[indexToSplitBy + index];
+    }
+
+    if (firstArray.length > 1) {
+      firstArray = MergeSort.merge(MergeSort.split(firstArray));
+    }
+    if (secondArray.length > 1) {
+      secondArray = MergeSort.merge(MergeSort.split(secondArray));
+    }
+
+    return new int[][]{ firstArray, secondArray };
+  }
+
+  public static int[] merge(int[][] arrays) {
+    int[] firstArray  = arrays[0];
+    int[] secondArray = arrays[1];
+    int firstArrayIndex  = 0;
+    int secondArrayIndex = 0;
+
+    int[] result = new int[firstArray.length + secondArray.length];
+    while (firstArrayIndex < firstArray.length || secondArrayIndex < secondArray.length) {
+      if (firstArrayIndex == firstArray.length) {
+        result[firstArrayIndex + secondArrayIndex] = secondArray[secondArrayIndex];
+        secondArrayIndex++;
+        continue;
+      }
+      if (secondArrayIndex == secondArray.length) {
+        result[firstArrayIndex + secondArrayIndex] = firstArray[firstArrayIndex];
+        firstArrayIndex++;
+        continue;
+      }
+      if (secondArray[secondArrayIndex] < firstArray[firstArrayIndex]) {
+        result[firstArrayIndex + secondArrayIndex] = secondArray[secondArrayIndex];
+        secondArrayIndex++;
+        continue;
+      }
+      if (firstArray[firstArrayIndex] < secondArray[secondArrayIndex]) {
+        result[firstArrayIndex + secondArrayIndex] = firstArray[firstArrayIndex];
+        firstArrayIndex++;
+        continue;
+      }
+    }
+
+    return result;
+  }
+
   public static int[] sort(int[] elements) {
-    return new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    return MergeSort.merge(MergeSort.split(elements));
   }
 }
--- src/algorithms/merge-sort/MergeSortTest.java
+++ src/algorithms/merge-sort/MergeSortTest.java
@@ -7,9 +7,82 @@ import org.junit.jupiter.api.Test;
 
 class MergeSortTest {
   @Test
-  public void doesSortWorks() {
+  public void doesSplitWorkCase1() {
+    Assertions.assertTrue(Arrays.deepEquals(
+      MergeSort.split(new int[]{ 7, 5, 10, 9, 2, 8, 1, 6, 4, 3 }),
+      new int[][]{
+        new int[]{ 2, 5, 7, 9, 10 },
+        new int[]{ 1, 3, 4, 6, 8  },
+      }
+    ));
+  }
+
+  @Test
+  public void doesSplitWorkCase2() {
+    Assertions.assertTrue(Arrays.deepEquals(
+      MergeSort.split(new int[]{ 7, 5 }),
+      new int[][]{
+        new int[]{ 7 },
+        new int[]{ 5 },
+      }
+    ));
+  }
+
+  @Test
+  public void doesSplitWorkCase3() {
+    Assertions.assertTrue(Arrays.deepEquals(
+      MergeSort.split(new int[]{ 7, 5, 10 }),
+      new int[][]{
+        new int[]{ 7     },
+        new int[]{ 5, 10 },
+      }
+    ));
+  }
+
+  @Test
+  public void doesMergeWorkCase1() {
+    Assertions.assertTrue(Arrays.equals(
+      MergeSort.merge(
+        new int[][]{
+          new int[]{ 7, 5, 10, 9, 2 },
+          new int[]{ 8, 1, 6 , 4, 3 },
+        }
+      ),
+      new int[]{ 7, 5, 8, 1, 6, 4, 3, 10, 9, 2 }
+    ));
+  }
+
+  @Test
+  public void doesMergeWorkCase2() {
+    Assertions.assertTrue(Arrays.equals(
+      MergeSort.merge(
+        new int[][]{
+          new int[]{ 7 },
+          new int[]{ 5 },
+        }
+      ),
+      new int[]{ 5, 7 }
+    ));
+  }
+
+  @Test
+  public void doesMergeWorkCase3() {
+    Assertions.assertTrue(Arrays.equals(
+      MergeSort.merge(
+        new int[][]{
+          new int[]{ 7     },
+          new int[]{ 5, 10 },
+        }
+      ),
+      new int[]{ 5, 7, 10 }
+    ));
+  }
+
+  @Test
+  public void doesSortWork() {
     int[] elements       = { 7, 5, 10, 9, 2, 8, 1, 6, 4, 3 };
     int[] elementsSorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    // int[] elementsSorted = { 7, 5, 8, 1, 6, 4, 3, 10, 9, 2 };
 
     Assertions.assertTrue(Arrays.equals(MergeSort.sort(elements), elementsSorted));
   }
