f1a02c1018c6e882e4fe4731c511cafc03f0856e
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-08 20:39:03 +0400

    typescript: Add conditional types example

--- src/typescript/conditional-types.ts
+++ src/typescript/conditional-types.ts
@@ -3,28 +3,35 @@ const name: ResultType<true> = "Jozeph"
 const age: ResultType<false> = 25
 
 /* Example of function argument type calculation based on the passed generic argument. */
-type Abc = { a: number; b: string; c: boolean; }
-type Ab = { a: number; b: string; }
-type A = { a: number; }
-type AbcAbATuple<T> = T extends Abc ? [number, string, boolean]
-    : T extends Ab ? [number, string]
-    : T extends A ? [number]
-    : never
-const AbArray: AbcAbATuple<{ a: number; }> = [1]
-function getConcatenatedElements<T> (elements: AbcAbATuple<T>) {
+type Abc = { a: number; b: string; c: boolean }
+type Ab = { a: number; b: string }
+type A = { a: number }
+type AbcAbATuple<T> = T extends Abc
+  ? [number, string, boolean]
+  : T extends Ab
+  ? [number, string]
+  : T extends A
+  ? [number]
+  : never
+const AbArray: AbcAbATuple<{ a: number }> = [1]
+function getConcatenatedElements<T>(elements: AbcAbATuple<T>) {
   return "|" + elements.join("|") + "|"
 }
-const concatenatedElementsA = getConcatenatedElements<{ a: number; }>([1])
-const concatenatedElementsAb = getConcatenatedElements<{ a: number; b: string; }>([1, "hehe"])
-const concatenatedElementsAbc = getConcatenatedElements<{ a: number; b: string; c: boolean}>([1, "hehe", true])
+const concatenatedElementsA = getConcatenatedElements<{ a: number }>([1])
+const concatenatedElementsAb = getConcatenatedElements<{ a: number; b: string }>([1, "hehe"])
+const concatenatedElementsAbc = getConcatenatedElements<{ a: number; b: string; c: boolean }>([1, "hehe", true])
 
 /* Calculation type based on another type. */
-type DateOrNumberOrString<T> = T extends Date ? Date
-  : T extends number ? Date | number
-  : T extends string ? Date | number | string
+type DateOrNumberOrString<T> = T extends Date
+  ? Date
+  : T extends number
+  ? Date | number
+  : T extends string
+  ? Date | number | string
   : never
 const compareDates = <T extends Date | number | string | boolean>(
-  firstDate: T, secondDate: DateOrNumberOrString<T>
+  firstDate: T,
+  secondDate: DateOrNumberOrString<T>
 ) => {
   return firstDate.toString() === secondDate.toString()
 }
@@ -40,5 +47,18 @@ const getIdPropertyValue = <T extends { id: any }>(input: T): T extends { id: in
   return input.id
 }
 const aVariableOfTypeNumber = getIdPropertyValue({ id: 123123 })
+
+type TypeName<T> = T extends string
+  ? "string"
+  : T extends number
+  ? "number"
+  : T extends boolean
+  ? "boolean"
+  : T extends undefined
+  ? "undefined"
+  : T extends Function
+  ? "function"
+  : "object"
+const myTypeName: TypeName<() => {}> = "function"
 
 export {}
