c2db12d3d3722e9cbf0c8cf93803cb2874d7ecb9
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-12 09:51:12 +0400

    algorithms: Init visualize tree method

--- src/algorithms/my-binary-search-tree/MyBinarySearchTree.java
+++ src/algorithms/my-binary-search-tree/MyBinarySearchTree.java
@@ -1,4 +1,35 @@
+class MyBinarySearchTreeNode {
+  public int value;
+  public MyBinarySearchTreeNode parentNode;
+  public MyBinarySearchTreeNode leftChildNode;
+  public MyBinarySearchTreeNode rightChildNode;
+
+  public MyBinarySearchTreeNode(
+    int value,
+    MyBinarySearchTreeNode parentNode,
+    MyBinarySearchTreeNode leftChildNode,
+    MyBinarySearchTreeNode rightChildNode
+  ) {
+    this.value = value;
+    this.parentNode = parentNode;
+    this.leftChildNode = leftChildNode;
+    this.rightChildNode = rightChildNode;
+  }
+}
+
 class MyBinarySearchTree {
+  private int height;
+  private MyBinarySearchTreeNode rootNode;
+
   public MyBinarySearchTree() {
+    this.height = 0;
+    this.rootNode = null;
+  }
+
+  public String visualize() {
+    if (this.rootNode == null) {
+      return "empty";
+    }
+    throw new RuntimeException("Error during visualization.");
   }
 }
--- src/algorithms/my-binary-search-tree/MyBinarySearchTreeTest.java
+++ src/algorithms/my-binary-search-tree/MyBinarySearchTreeTest.java
@@ -1,11 +1,14 @@
 // To run this test:
 // javac -cp ./lib/junit-platform-console-standalone-1.7.0-all.jar ./src/algorithms/my-binary-search-tree/MyBinarySearchTree.java ./src/algorithms/my-binary-search-tree/MyBinarySearchTreeTest.java && java -jar ./lib/junit-platform-console-standalone-1.7.0-all.jar -cp ./src/algorithms/my-binary-search-tree --select-class MyBinarySearchTreeTest && rm ./src/**/*.class
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MyBinarySearchTreeTest {
   @Test
-  public void testIsSizeSetUpCorrectly() {
+  public void doesItWorkCorrectly() {
     MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+
+    Assertions.assertEquals("empty", myBinarySearchTree.visualize());
   }
 }
